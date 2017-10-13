5c4c2b3f64127fcb0197f879a5c18f25e741655d
= <=>
2017-10-13 18:56:35 +0800

    Update database .sql file

--- FYPNANI/ttms.sql
+++ FYPNANI/ttms.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2017 at 08:45 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.5.38
+-- Generation Time: Oct 13, 2017 at 12:55 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -125,17 +127,21 @@ INSERT INTO `lecturer` (`lect_id`, `lect_name`) VALUES
 --
 
 CREATE TABLE `lect_register` (
+  `id` int(11) NOT NULL,
   `lect_id` int(11) NOT NULL,
   `batch_id` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL
+  `class_id` int(11) NOT NULL,
+  `sub_id` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lect_register`
 --
 
-INSERT INTO `lect_register` (`lect_id`, `batch_id`, `class_id`) VALUES
-(0, 0, 0);
+INSERT INTO `lect_register` (`id`, `lect_id`, `batch_id`, `class_id`, `sub_id`) VALUES
+(5, 34, 37, 2, 'DIT6777'),
+(6, 34, 37, 2, 'DIT7655'),
+(7, 35, 37, 3, 'DIT7655');
 
 -- --------------------------------------------------------
 
@@ -209,8 +215,8 @@ ALTER TABLE `lecturer`
 -- Indexes for table `lect_register`
 --
 ALTER TABLE `lect_register`
-  ADD PRIMARY KEY (`lect_id`),
-  ADD KEY `lect_id` (`lect_id`,`batch_id`,`class_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lect_id` (`batch_id`,`class_id`);
 
 --
 -- Indexes for table `subject`
@@ -234,31 +240,44 @@ ALTER TABLE `time`
 --
 ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `batch`
 --
 ALTER TABLE `batch`
   MODIFY `batch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
 --
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
   MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `day`
 --
 ALTER TABLE `day`
   MODIFY `day_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `lecturer`
 --
 ALTER TABLE `lecturer`
   MODIFY `lect_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `lect_register`
+--
+ALTER TABLE `lect_register`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT for table `time`
 --
 ALTER TABLE `time`
   MODIFY `time_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
